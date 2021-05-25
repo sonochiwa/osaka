2853a5dc2970ad6f9ea95c0106fc6bc36b30fba6
sonochiwa <sonochiwaxd@gmail.com>
2021-05-25 16:41:30 +0600

    urls fix

--- main/urls.py
+++ main/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('osaka.urls')),
-    path('account', include('account.urls')),
-    path('catalog', include('catalog.urls', namespace='catalog')),
+    path('account/', include('account.urls')),
+    path('catalog/', include('catalog.urls', namespace='catalog')),
 ]
 
 if settings.DEBUG:
