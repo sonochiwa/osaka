2ed22fb689d35eade06c9df6fe1ce606e072806b
sonochiwa <sonochiwaxd@gmail.com>
2021-06-06 20:33:39 +0600

    profile, contacts hotfix

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-from django.views.generic import CreateView
 from .models import Contact
 from .forms import ContactForm
+from django.views.decorators.http import require_POST
+from django.http import HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
 
-
-class ContactView(CreateView):
-    model = Contact
-    form_class = ContactForm
-    success_url = '/'
+@require_POST
+def subscription(request):
+    cantact_form = ContactForm(request.POST)
+    if cantact_form.is_valid():
+        cd = cantact_form.cleaned_data
+        email = cd["email"]
+        try:
+            contact = Contact.objects.get(email=email)
+        except ObjectDoesNotExist:
+            cantact_form.save()
+    return HttpResponseRedirect("/")
