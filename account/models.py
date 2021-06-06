cbebe3046f716ea40e9ed202a534d3ddb9eed1e2
sonochiwa <sonochiwaxd@gmail.com>
2021-06-06 20:10:12 +0600

    profile hotfix

--- account/models.py
+++ account/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True, null=True)
 
     def str(self):
         return 'Profile for user {}'.format(self.user.username)
