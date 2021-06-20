421ccbdc0e97743b4e97a84fd73720bdc023cdb0
sonochiwa <sonochiwaxd@gmail.com>
2021-06-20 22:16:41 +0600

    update

--- osaka/views.py
+++ osaka/views.py
@@ -6,6 +6,8 @@ def index(request):
     discount_products = Product.objects.filter(discount__gt=0)[:8]
     return render(request, 'osaka/index.html', context={'section': 'index', 'products': discount_products})
     
-
 def about(request):
     return render(request, 'osaka/about.html', context={'section': 'about'})
+
+def track(request):
+    return render(request, 'osaka/track.html', context={'section': 'track'})
